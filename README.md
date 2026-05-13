0b65e0bb8056d71809da996b31fa4b63b23e1c6f
Irpan Kusuma Wijaya <Irpan.Wijaya@cimbniaga.co.id>
2026-05-13 23:19:44 +0700

    initial

---
+++ README.md
@@ -0,0 +1,52 @@
+Prerequisites:
+
+- [Vercel CLI](https://vercel.com/docs/cli) installed globally
+
+To develop locally:
+
+```
+npm install
+vc dev
+```
+
+```
+open http://localhost:3000
+```
+
+To build locally:
+
+```
+npm install
+vc build
+```
+
+To deploy:
+
+```
+npm install
+vc deploy
+```
+
+
+┌  🚀 Creating a Prisma Postgres database
+│
+◇  Database created successfully!
+│
+│
+●  Database Connection
+│
+│
+│    Connection String:
+│
+│    postgres://e43d155feb1fcd69f8683e3e1070b18014a75c356dd323f1c886b195e393b28c:sk_OFn1JGzntGtkeWDKBhKO8@db.prisma.io:5432/postgres?sslmode=require
+│
+│
+◆  Claim Your Database
+│
+│    Keep your database for free:
+│
+│    https://create-db.prisma.io/claim?projectID=proj_cmp47o4460bsy1af82o8d3q8u&utm_source=create-db&utm_medium=cli
+│
+│    Database will be deleted on 5/14/2026, 10:25:15 PM if not claimed.
+│
+└  Done!
---
+++ src/config.ts
@@ -0,0 +1,41 @@
+import { configDotenv } from "dotenv";
+import { cleanEnv, json, num, str } from "envalid";
+
+configDotenv({
+    quiet: true,
+    path: [
+        '.env',
+        '.env.local'
+    ]
+})
+
+export enum Environments {
+    PRODUCTION = 'production',
+    DEV = 'development',
+    TEST = 'test',
+    STAGING = 'staging',
+}
+
+const config = {
+    NODE_ENV: str({
+        default: Environments.DEV,
+        choices: [...Object.values(Environments)],
+    }),
+    PORT: num({ default: 9000 }),
+    
+    JWT_SECRET: str({ default: 'jwt-secret' }),
+    JWT_EXPIRE: str({ default: '30m' }),
+    E_SALT: str(),
+    
+    DATABASE_URL: str({ default: 'postgres://personal:Pers0nal@10.121.75.73:5432/personal?schema=public' }),
+    FTP_HOST: str({ default: 'localhost' }),
+    FTP_PORT: num({ default: 990 }),
+    FTP_USERNAME: str(),
+    FTP_PASSWORD: str(),
+    FTP_CONFIG: json({ desc: `{ secure: boolean; }` }),
+
+    GOOGLE_CLIENT_ID: str(),
+    GOOGLE_CLIENT_SECRET: str(),
+}
+
+export const env = cleanEnv(process.env, config)
